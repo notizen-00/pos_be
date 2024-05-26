f8d876613b97e9cb20d08129aab91901ccf7a9c7
dipta ikromi m <dipta.ikromi@unej.ac.id>
2024-05-26 07:53:50 +0700

    build(deps): update 3 files and create 1 file

--- app/Models/KategoriProduk.php
+++ app/Models/KategoriProduk.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class KategoriProduk extends Model
 {
@@ -18,5 +19,28 @@ class KategoriProduk extends Model
     public function produk()
     {
         return $this->hasMany(Produk::class);
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
+
+    // Mutator for created_at
+    public function setCreatedAtAttribute($value)
+    {
+        $this->attributes['created_at'] = Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
+
+    // Accessor for updated_at
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
+
+    // Mutator for updated_at
+    public function setUpdatedAtAttribute($value)
+    {
+        $this->attributes['updated_at'] = Carbon::parse($value)->format('Y-m-d H:i:s');
     }
 }
--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Scopes\KategoriDetailScope;
+use Carbon\Carbon;
 
 class Produk extends Model
 {
     use HasFactory;
     protected $table = 'produk';
+
     protected $fillable = [
         'nama',
         'harga',
@@ -18,9 +21,35 @@ class Produk extends Model
         'foto',
         'favorit'
     ];
-
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new kategoriDetailScope);
+    }
     public function kategori()
     {
         return $this->belongsTo(KategoriProduk::class);
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
+
+    // Mutator for created_at
+    public function setCreatedAtAttribute($value)
+    {
+        $this->attributes['created_at'] = Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
+
+    // Accessor for updated_at
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
+
+    // Mutator for updated_at
+    public function setUpdatedAtAttribute($value)
+    {
+        $this->attributes['updated_at'] = Carbon::parse($value)->format('Y-m-d H:i:s');
     }
 }
